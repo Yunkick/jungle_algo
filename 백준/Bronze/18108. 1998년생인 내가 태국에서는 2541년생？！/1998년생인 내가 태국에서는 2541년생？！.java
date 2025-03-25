fb52fa7701d8d343ea67ccce599ddfd0ae61a5ea
Yunkick <114406361+Yunkick@users.noreply.github.com>
2025-03-25 23:54:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 220 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner a  = new Scanner (System.in);
+		int y = a.nextInt();
+		System.out.println( y - 543);
+		
+
+	}
+
+}
