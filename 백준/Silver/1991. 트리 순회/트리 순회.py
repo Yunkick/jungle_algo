5cab604f0c70c79383898565b85af82858d491f6
Yunkick <114406361+Yunkick@users.noreply.github.com>
2025-03-31 11:32:02 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,33 +1,58 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.nodes = {}
+    
+    def add_node(self,root,left,right):
+        if root not in self.nodes:
+            self.nodes[root] = Node(root)
+        
+
+        if left != ".":
+            self.nodes[left] = Node(left)
+            self.nodes[root].left = self.nodes[left]
+            
+        
+        if right != ".":
+            self.nodes[right] = Node(right)
+            self.nodes[root].right = self.nodes[right]
+            
+
+    def pre_order(self, node):
+        if node:
+            print(node.value, end="")
+            self.pre_order(node.left)
+            self.pre_order(node.right)
+
+    def in_order(self, node):
+        if node:
+            self.in_order(node.left)
+            print(node.value, end="")
+            self.in_order(node.right)
+
+    def post_order(self, node):
+        if node:
+            self.post_order(node.left)
+            self.post_order(node.right)
+            print(node.value, end="")   
+
 n = int(input())
-tree = {}
+tree = BinaryTree()
 
-for i in range(n):
-    root, left , right = map(str,input().split())
-    tree[root] = left, right
+for _ in range(n):
+    root, left, right = input().split()
+    tree.add_node(root,left,right)
 
+root_node = tree.nodes['A']
 
-def pre_order(v):
-    if v != ".":
-        print(v, end="")
-        pre_order(tree[v][0])
-        pre_order(tree[v][1])
-
-def mid(v):
-    if v != ".":
-        mid(tree[v][0])
-        print(v, end="")
-        mid(tree[v][1])
-
-def 후위(v):
-    if v != ".":
-        후위(tree[v][0])
-        후위(tree[v][1])
-        print(v, end="")
-
-
-pre_order('A')
-print("")
-mid('A')
-print("")
-후위('A')
-print("")
+tree.pre_order(root_node)
+print()
+tree.in_order(root_node)
+print()
+tree.post_order(root_node)
+print()
